fb91f6e4f970896c701962a0426fca97bd89e6a5
Cyzer <icegamer.82@yahoo.com>
2023-11-22 16:49:58 +0330

    hint add for later changes

--- Domain/CoreServices/CoreService.cs
+++ Domain/CoreServices/CoreService.cs
@@ -27,13 +27,19 @@ namespace Domain.CoreServices
         public IQueryable<T> Table() 
         {
             var parameter = Expression.Parameter(typeof(T), "Entity");
-            var property = Expression.Property(parameter, "Ddate");
-            var body = Expression.Equal(property, Expression.Constant(null));
+            var PI_Ddate = Expression.Property(parameter, "Ddate");
+            var body = Expression.Equal(PI_Ddate, Expression.Constant(null));
 
             var nullDdateExpression = Expression.Lambda<Func<T, bool>>(body, parameter);
             
             return dbTable.Where(nullDdateExpression).AsNoTracking();
         }
+        /*
+        var body = Expression.Or(
+              Expression.Equal(PI_DuserId, Expression.Constant(null)),
+              Expression.Equal(PI_Ddate, Expression.Constant(null))
+            );
+        */
 
     }
 }
