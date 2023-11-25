12f23b329c0bb9a121275aca872dc6cc8884e06c
Cyzer <icegamer.82@yahoo.com>
2023-11-25 16:45:30 +0330

    change arch, note: services are still using Domain.CoreService, TODO: change service installers to add CoreLayer.Service

---
+++ CoreLayer/Interfaces/ICoreService.cs
@@ -0,0 +1,43 @@
+﻿using Domain.ModelMetadata;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLayer.Interfaces
+{
+    public interface ICoreService<T> where T : BaseModel
+    {
+            public IQueryable<T> Table();
+
+            public IQueryable<TEntity> Table<TEntity>() where TEntity : BaseModel;
+
+            public IQueryable<T> TableAll();
+
+        public IQueryable<TEntity> TableAll<TEntity>() where TEntity : BaseModel;
+
+            public Task<T?> FindByIdAsync(long id);
+
+            public Task Create(T T, bool save = true);
+
+            public Task Delete(long id, bool save = true);
+
+            public Task Delete(T InputEntity, bool save = true);
+
+            public Task Update(T InputEntity, bool save = true);
+
+
+            public Task CommitAsync();
+
+            public Task BeginTransaction();
+
+            public Task CommitTransaction();
+
+            public Task RollBackTransaction();
+        
+    }
+}
+
---
+++ CoreLayer/Utils/GridData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLayer.Utils
+{
+    public class GridData<T> where T : class
+    {
+        public List<T> Data { get; set; } = new List<T>();
+        public long pageNumber { get; set; } = 1;
+        public long pageSize { get; set; } = long.MaxValue;
+        public long totalPages { get; set; }
+        public long totalCount { get; set; }
+        public List<Filter> filters { get; set; } = new List<Filter>();
+    }
+}
