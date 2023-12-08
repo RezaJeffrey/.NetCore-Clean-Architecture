2923eb70c47102df14aed18f6edbece36a9b1a0a
Cyzer <icegamer.82@yahoo.com>
2023-12-08 15:24:43 +0330

    [feat] Implement Authetication app Service schema

---
+++ Application/Services/AuthenticationService.cs
@@ -0,0 +1,42 @@
+﻿using CoreLayer.Services;
+using Domain.DTOs;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class AuthenticationService
+    {
+        private readonly CoreService<User, UserDTO> CoreService;
+        private readonly AuthUcService AuthUcService;
+        public AuthenticationService(AuthUcService authUcService, CoreService<User, UserDTO> coreService)
+        {
+            CoreService = coreService;
+            AuthUcService = authUcService;
+        }
+
+        public async Task<string> GetAccessToken()
+        {
+            throw new NotImplementedException();
+        }
+        
+        public async Task<string> RefreshToken()
+        {
+            throw new NotImplementedException();
+        }
+        
+        public async Task<string> Login()
+        {
+            throw new NotImplementedException();
+        }
+        public async Task<string> Register()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
