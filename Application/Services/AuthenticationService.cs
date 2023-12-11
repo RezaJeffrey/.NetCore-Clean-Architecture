c8e94e70bab9b9e3cca77141abd5ef69a68b22cf
Cyzer <icegamer.82@yahoo.com>
2023-12-11 09:57:19 +0330

    [feat, clean] clean auth service, add lambda expressions for coreService

--- Application/Services/AuthenticationService.cs
+++ Application/Services/AuthenticationService.cs
@@ -47,7 +47,9 @@ namespace Application.Services
 
             long userId = long.Parse(deserialized_token.GetClaim("UserId").Value);
             User? user = await CoreService.FindByIdAsync(userId);
-            if (user == null) throw new AppRuleException("User Not Found!");
+
+            if (user == null)
+                throw new AppRuleException("User Not Found!");
 
             var userRoles = CoreService.Table<UserRole>()
                 .Include(ur => ur.Role)
@@ -61,28 +63,37 @@ namespace Application.Services
         public async Task<string> GetAccessToken(AuthDTO user_dto)
         {
             User? user = CoreService.Table()
-                .Include(u => u.MainRole).ThenInclude(mr => mr.Role)
-                .Include(u => u.UserRoles).ThenInclude(ur => ur.Role)
+                .Include(u => u.MainRole)
+                    .ThenInclude(mr => mr.Role)
+                .Include(u => u.UserRoles)
+                    .ThenInclude(ur => ur.Role)
                 .FirstOrDefault(u => u.UserName == user_dto.UserName);
 
-            if (user == null) throw new AppRuleException("Wrong username or password");
+            if (user == null)
+                throw new AppRuleException("Wrong username or password");
+
 
             // TODO log login attempts
             var Now = DateTime.Now.Ticks;
             var logs = CoreService.Table<LogLogin>()
                 .Where(l => l.UserId == user.Id && l.ExpDate > Now && !l.IsSuccess);
+
             if (logs.Count() > 5) 
                 throw new AppRuleException(
-                        "account have been limited due to 5 failed login attempts, please try again later or Inform us"
+                        "Account has been limited due to 5 failed login attempts. Please try again later or inform us."
                     );
 
-            LogLogin log = new LogLogin();
-            log.UserId = user.Id;
-            log.IpAddress = AuthUtilService.GetClientIp();
-            log.ExpDate = DateTime.Now.AddDays(1).Ticks;
+            LogLogin log = new LogLogin
+            {
+                UserId = user.Id,
+                IpAddress = AuthUtilService.GetClientIp(),
+                ExpDate = DateTime.Now.AddDays(1).Ticks
+            };
 
 
-            bool passwordMatch = await AuthUtilService.ValidatePassword(user_dto.Password, user.PasswordSalt, user.PasswordHash);
+            bool passwordMatch = await AuthUtilService
+                .ValidatePassword(user_dto.Password, user.PasswordSalt, user.PasswordHash);
+            
             if (!passwordMatch)
             {
                 // Insert log
@@ -94,13 +105,8 @@ namespace Application.Services
                 
 
             var userRoles = user.UserRoles.Select(ur => ur.Role).ToList();
-                //await CoreService.Table<UserRole>()
-                //.Include(ur => ur.Role)
-                //.Where(ur => ur.UserId == user.Id)
-                //.Select(ur => ur.Role)
-                //.ToListAsync();
-
             var token = AuthUcService.CreateToken(user, userRoles);
+
             log.IsSuccess = true;
             await LoginLogService.CoreService.Create(log);
 
@@ -112,15 +118,17 @@ namespace Application.Services
             {
                 await CoreService.BeginTransaction();
 
-                if (user_dto.Password != user_dto.PasswordRepeat) throw new AppRuleException("Passwords must match");
+                if (user_dto.Password != user_dto.PasswordRepeat)
+                    throw new AppRuleException("Passwords must match");
 
-                bool userExists = CoreService.Table().FirstOrDefault(user => user.UserName == user_dto.UserName) != null;
+                bool userExists = CoreService.Table()
+                    .FirstOrDefault(user => user.UserName == user_dto.UserName) != null;
+
                 if (userExists)
                     throw new AppRuleException(
                             "a user with this username already exists, Consider Login or enter new username"
                         );
 
-                // TODO add user and 
 
                 var RegisterUser = ObjectMapper.MapObject<AuthDTO, User>(user_dto);
 
@@ -128,6 +136,7 @@ namespace Application.Services
 
                 var main_role = CoreService.Table<Role>()
                     .FirstOrDefault(role => role.Gcode.ToString() == user_dto.MainRoleGcode);
+                
                 if (main_role == null)
                     throw new AppRuleException("Role is not valid");
 
@@ -144,6 +153,7 @@ namespace Application.Services
                     .FirstOrDefaultAsync(
                             user => user.UserName.ToLower() == RegisterUser.UserName.ToLower()
                         );
+                
                 if (createdUser == null) throw new AppRuleException("User Creation Failed"); // TODO test failure
 
                 List<Role> roles = new List<Role>();
@@ -151,7 +161,9 @@ namespace Application.Services
                 if (user_dto.rolesToRegister.Any())
                 {
                     var roleAvailable = await RoleService.CheckRoleAvailable(user_dto.rolesToRegister.ToList());
-                    if (!roleAvailable) throw new AppRuleException("Input role not valid");
+                    
+                    if (!roleAvailable)
+                        throw new AppRuleException("Input role not valid");
 
                     var roles_to_add = user_dto.rolesToRegister
                         .Where(r => r != null)
@@ -159,7 +171,6 @@ namespace Application.Services
                         .ToList();
 
                     roles = await UserRoleService.AddUserRole(createdUser.Id, roles_to_add);
-
                 }
                 else
                 {
@@ -168,11 +179,13 @@ namespace Application.Services
                 }
 
                 var exists = roles.Where(r => r.Gcode == createdUser.MainRole?.Role.Gcode).Any();
+                
                 if (!exists)
                     throw new AppRuleException("User MainRole doesn't match selected roles to add");
 
                 await CoreService.CommitAsync();
                 await CoreService.CommitTransaction();
+
                 return AuthUcService.CreateToken(createdUser, roles);
             }
             catch(Exception)
@@ -182,16 +195,5 @@ namespace Application.Services
             }
         }
 
-        public async Task<bool> TestToken()
-        {
-            var token = AuthUtilService.GetUserToken();
-            var clientIp = AuthUtilService.GetClientIp();
-            var userROle = AuthUtilService.GetUserRole();
-            var claims = AuthUtilService.getClaims();
-            var logedInUserID = AuthUtilService.getUserId();
-
-
-            return true;
-        }
     }
 }
---
+++ Utils/Expressions/CoreExpression.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq.Expressions;
+
+namespace Utils.Expressions
+{
+    public class CoreExpression<T> where T : class
+    {
+        public Expression<Func<T, bool>> EntityIdNullExpression()
+        {
+            var parameter = Expression.Parameter(typeof(T), "Entity");  // Entity
+            var property = Expression.Property(parameter, "Id");  // Entity.Id
+            var body = Expression.Equal(property, Expression.Constant(null)); // Entity.Id == null
+
+            var expression = Expression.Lambda<Func<T, bool>>(body, parameter);
+
+            return expression;
+        }
+
+        public Expression<Func<T, long>> EntityId()
+        {
+            var parameter = Expression.Parameter(typeof(T), "Entity");  // Entity
+            var property = Expression.Property(parameter, "Id");  // Entity.Id
+            var body = Expression.Lambda<Func<T, long>>(property, parameter);  // Entity => Entity.Id
+
+            return body;
+        }
+
+    }
+}
