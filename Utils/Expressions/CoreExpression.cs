b7c23270440fdcc28c1a19cc20684a09516161de
Cyzer <icegamer.82@yahoo.com>
2023-12-11 10:07:55 +0330

    [feat] Implement Expressions for coreService[NotTested]

--- Utils/Expressions/CoreExpression.cs
+++ Utils/Expressions/CoreExpression.cs
@@ -2,26 +2,40 @@
 
 namespace Utils.Expressions
 {
-    public class CoreExpression<T> where T : class
+    public static class CoreExpression<T> where T : class
     {
-        public Expression<Func<T, bool>> EntityIdNullExpression()
+        public static Expression<Func<T, bool>> EntityIdZeroOrNullExpression()
         {
             var parameter = Expression.Parameter(typeof(T), "Entity");  // Entity
             var property = Expression.Property(parameter, "Id");  // Entity.Id
-            var body = Expression.Equal(property, Expression.Constant(null)); // Entity.Id == null
 
-            var expression = Expression.Lambda<Func<T, bool>>(body, parameter);
+            var Equalbody = Expression.Equal(property, Expression.Constant(null)); // Entity.Id == null
+            var ZeroBody = Expression.Equal(property, Expression.Constant((long)0)); // Entity.Id == 0
+            var OrBody = Expression.Or(Equalbody, ZeroBody); 
+
+            var expression = Expression.Lambda<Func<T, bool>>(OrBody, parameter);
 
             return expression;
         }
 
-        public Expression<Func<T, long>> EntityId()
+        public static Expression<Func<T, long>> EntityIdExpression()
         {
             var parameter = Expression.Parameter(typeof(T), "Entity");  // Entity
             var property = Expression.Property(parameter, "Id");  // Entity.Id
             var body = Expression.Lambda<Func<T, long>>(property, parameter);  // Entity => Entity.Id
 
             return body;
+        }
+
+        public static Expression<Func<T, bool>> EntityFindByIdExpression(long Id)
+        {
+            var parameter = Expression.Parameter(typeof(T), "Entity");  
+            var property = Expression.Property(parameter, "Id");
+            var body = Expression.Equal(property, Expression.Constant(Id));
+
+            var expression = Expression.Lambda<Func<T, bool>>(body, parameter);
+
+            return expression;
         }
 
     }
